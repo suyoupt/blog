9ddbee05e58706f0e50605713660d2387503ca7b
suyoupt <470361490@qq.com>
2017-10-21 16:54:23 +0800

    Post creation, modification, listing added

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -7,8 +7,11 @@
  */
 
 namespace AppBundle\Controller;
+use AppBundle\Entity\Post;
+use AppBundle\Form\PostType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 
@@ -24,6 +27,51 @@ class PostController extends Controller
 
         return $this->render("post/index.html.twig", [
             "posts" => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/post/new", name="post_new")
+     */
+    public function newAction(Request $request)
+    {
+        $post = new Post();
+        $form = $this->createForm("AppBundle\Form\PostType", $post);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+            return $this->redirectToRoute("post_index");
+        }
+
+        return $this->render("post/new.html.twig", [
+            "form" => $form->createView(),
+            "post" => $post
+        ]);
+    }
+
+    /**
+     * @Route("/post/{id}/edit", name="post_edit")
+     */
+    public function editAction(Request $request, Post $post)
+    {
+        $form = $this->createForm("AppBundle\Form\PostType", $post);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+            return $this->redirectToRoute("post_index");
+        }
+
+        return $this->render("post/edit.html.twig", [
+            "form" => $form->createView(),
+            "post" => $post
         ]);
     }
 }
