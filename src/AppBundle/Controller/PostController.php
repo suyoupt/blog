21b4872b3b7a15372b05050d71b57bf29908b678
suyoupt <470361490@qq.com>
2017-10-28 16:41:20 +0800

    post delete and show feature added

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -13,6 +13,7 @@ use Doctrine\ORM\QueryBuilder;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -28,8 +29,10 @@ class PostController extends Controller
     /**
      * @Route("/post/", name="post_index")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $word = $request->query->get('word');
+
         $em = $this->getDoctrine()->getManager();
        // $posts = $this->getDoctrine()->getRepository("AppBundle:Post")->findAll();
         /** @var QueryBuilder $qb */
@@ -40,12 +43,16 @@ class PostController extends Controller
 //            ->getQuery()
 //        ;
 
-        $posts = $this->getDoctrine()->getRepository("AppBundle:Post")->getByUser($this->getUser()->getId());
+        $posts = $this->getDoctrine()->getRepository("AppBundle:Post")->getByUserAndKeyword(
+            $this->getUser()->getId(),
+            $word
+        );
 
 
 
         return $this->render("post/index.html.twig", [
-            "posts" => $posts
+            "posts" => $posts,
+            "searchForm" => $this->createSearchForm()->createView()
         ]);
     }
 
@@ -86,7 +93,7 @@ class PostController extends Controller
             $em = $this->getDoctrine()->getManager();
             $em->persist($post);
             $em->flush();
-            return $this->redirectToRoute("post_index");
+            return $this->redirectToRoute("post_show", [ "id" => $post->getId()]);
         }
 
         return $this->render("post/edit.html.twig", [
@@ -98,19 +105,95 @@ class PostController extends Controller
     /**
      * @Route("/post/{id}/publish", name="post_publish")
      *
+     * @Method("PATCH")
      */
     public function publishAction(Request $request, Post $post)
     {
-        $em = $this->getDoctrine()->getManager();
-        $post->setStatus(POst::STATUS_PUBLISHED);
-        //$em->persist($post);
-        $em->flush();
+
+        $form = $this->createPublishForm($post);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $post->setStatus(POst::STATUS_PUBLISHED);
+            //$em->persist($post);
+            $em->flush();
+        }
+
         return $this->redirectToRoute("post_index");
 //        $form = $this->createFormBuilder()
 //            ->setAction($this->generateUrl("post_publish", array( "id" => $post->getId())))
 //            ->setMethod("post")
 //            ->getForm();
 
+    }
+
+    /**
+     *
+     * @Route("/post/{id}/show", name="post_show")
+     * @param Request $request
+     * @param Post $post
+     * @return Response
+     */
+    public function showAction(Request $request, Post $post)
+    {
+
+        return $this->render("post/show.html.twig", [
+            "post" => $post,
+            "deleteForm" => $this->createDeleteForm($post)->createView(),
+            "publishForm" => $this->createPublishForm($post)->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/post/{id}/delete", name="post_delete")
+     * @Method("DELETE")
+     *
+     * @param Request $request
+     * @param Post $post
+     */
+    public function deleteAction(Request $request, Post $post)
+    {
+        $form = $this->createDeleteForm($post);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($post);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl("post_index"));
+    }
+
+    /**
+     * @param Post $post
+     * @return \Symfony\Component\Form\Form|\Symfony\Component\Form\FormInterface
+     */
+    protected function createDeleteForm(Post $post)
+    {
+        $form = $this->createFormBuilder()
+            ->setAction($this->generateUrl("post_delete", ["id" => $post->getId()]))
+            ->setMethod("DELETE")
+            ->getForm()
+        ;
+
+        return $form;
+    }
+
+    protected function createPublishForm(Post $post)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl("post_publish", ["id" => $post->getId()]))
+            ->setMethod("PATCH")
+            ->getForm()
+            ;
+    }
+
+    protected function createSearchForm()
+    {
+        return $this->createFormBuilder()
+            ->add("word", TextType::class)
+            ->setAction($this->generateUrl("post_index"))
+            ->setMethod("GET")
+            ->getForm();
     }
 
 }
--- src/AppBundle/Repository/PostRepository.php
+++ src/AppBundle/Repository/PostRepository.php
@@ -8,12 +8,20 @@ namespace AppBundle\Repository;
  */
 class PostRepository extends \Doctrine\ORM\EntityRepository
 {
-    public function getByUser($user_id)
+    public function getByUserAndKeyword($user_id, $word)
     {
         $qb = $this->createQueryBuilder('p')
             ->leftJoin('p.createdBy', 'c')
             ->where('c.id = :id')
             ->setParameter('id', $user_id);
+
+
+        if (!empty($word)) {
+            $qb
+                ->andWhere('p.title LIKE :word OR p.body LIKE :word')
+                ->setParameter('word', "%$word%");
+        }
+
 
         return $qb->getQuery()->getResult();
 
